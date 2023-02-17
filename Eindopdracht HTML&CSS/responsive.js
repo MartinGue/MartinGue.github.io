7ee2d25c7dad5b80c2d87b9430b8422a0a305b23
MG2022 <10505124+MartinGue@users.noreply.github.com>
2023-02-17 14:26:32 +0100

    corrections

--- Eindopdracht HTML&CSS/responsive.js
+++ Eindopdracht HTML&CSS/responsive.js
@@ -11,25 +11,20 @@ function myFunction() {
 }
 
 
-// The sticky menu //
-// window.onscroll = function () { myFunction2() };
-// var navbar = document.getElementById("myTopnav");
-// var sticky = navbar.offsetTop;
-
-// function myFunction2() {
-//   if (window.pageYOffset >= sticky) {
-//     navbar.classList.add("sticky")
-//   } else {
-//     navbar.classList.remove("sticky");
-//   }
-// }
-
-// Get the button, and when the user clicks on it, execute myFunction//
-document.getElementById("myBtn").onclick = function () { myFunction3() };
-
-/* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
-function myFunction3() {
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function showDropdown() {
   document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
 }
 
 
